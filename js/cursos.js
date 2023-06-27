67d4f7feab341629e72262c05a74ff32d814fab6
Exequiel Pagliari <58015928+exequielpagliari@users.noreply.github.com>
2023-06-27 12:40:48 -0300

    Mejorado el CSS de Capacitación

--- js/cursos.js
+++ js/cursos.js
@@ -1,16 +1,12 @@
-const app = Vue.createApp({
+const app1 = Vue.createApp({
 
     
 
     data() {
         return {
-            /*cursos: [['LINKPOSCOSECHA','Finalizado','introposco.png','Intro a la poscosecha','Introducción a la poscosecha'],['LINKPOSCOSECHA','Finalizado','introposco.png','Intro a la poscosecha','Introducción a la poscosecha'],['LINKPOSCOSECHA','Finalizado','introposco.png','Intro a la poscosecha','Introducción a la poscosecha']],*/
-            cursos: "",
-            link: 'LINKPOSCOSECHA',
-            estado: 'Finalizado',
-            imagen: 'introposco.png',
-            imagentexto: 'Intro a la poscosecha',
-            curso: 'Introducción a la poscosecha',
+            
+            cursos: [],
+
         }
     },
     /*Cuando ubicamos el módulo de comunicación
@@ -30,4 +26,4 @@ const app = Vue.createApp({
 
 }
 
-}).mount("#app")
+}).mount("#app1")
